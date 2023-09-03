ee1f18e35a1c12d25f8ff9454c99515735f8226f
RadMa <maks25022@gmail.com>
2023-09-03 14:02:29 +0500

    Fixed 2

--- 2/Program2.cs
+++ 2/Program2.cs
@@ -1,12 +1,12 @@
 ﻿void FindNum(int[,] matrix, int num)
 {
-    if (matrix.GetLength(1) * matrix.GetLength(0) >= num)
+    if (matrix.Length >= num)
     {
         int row = num % matrix.GetLength(1);
-        int column = (num - row) / matrix.getLength(1);
-        Console.WriteLine(%"Value positioned at {num} position is {matrix[column, row]}")
+        int column = (num - row) / matrix.GetLength(1);
+        Console.WriteLine($"Value positioned at {num} position is {matrix[column, row]}");
     }
-    else { Console.WriteLine("Given number is out of bounds")}
+    else { Console.WriteLine("Given number is out of bounds"); }
 }
 int[,] mat = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
 FindNum(mat, 4);
